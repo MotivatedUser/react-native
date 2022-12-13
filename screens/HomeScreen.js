9ed4a50576a870ae9c094ebfb1b46d499e110f86
MotivatedUser <carbfixer@gmail.com>
2022-12-13 09:59:57 -0800

    Activity Indicator

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,9 +3,18 @@ import { Text, View, ScrollView } from "react-native";
 import { Card } from "react-native-elements";
 import { useSelector } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import Loading from "../components/LoadingComponent";
 
 
-const FeaturedItem = ({ item }) => {
+const FeaturedItem = (props) => {
+    const {item} = props;
+
+    if(props.isLoading) {
+        return <Loading />
+    }
+    if (props.errMess) {
+        return <Text>{props.errMess}</Text>
+    }
     if (item) {
         return (
             <Card containerStyle={{ padding: 0 }} >
@@ -36,9 +45,9 @@ const HomeScreen = () => {
 
     return (
         <ScrollView>
-            <FeaturedItem item={featCampsite} />
-            <FeaturedItem item={featPromotion} />
-            <FeaturedItem item={featPartner} />
+            <FeaturedItem item={featCampsite} isLoading={campsites.isLoading} errMess={campsites.errMess} />
+            <FeaturedItem item={featPromotion} isLoading={promotions.isLoading} errMess={promotions.errMess} />
+            <FeaturedItem item={featPartner} isLoading={partners.isLoading} errMess={partners.errMess} />
         </ScrollView>
     );
 };
